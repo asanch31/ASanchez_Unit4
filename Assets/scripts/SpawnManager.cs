09061b9876ada943b73b08147a5961f27d90e86f
asanch31 <78468358+asanch31@users.noreply.github.com>
2021-04-21 15:30:08 -0700

    Unit 4 lesson D
    Added waves of enemies,

--- Assets/scripts/SpawnManager.cs
+++ Assets/scripts/SpawnManager.cs
@@ -5,15 +5,28 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public GameObject powerUpPrefab;
+
     private float spawnRange = 8.5f;
+    private int enemyCount;
+    private int waveNum = 1;
+    
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        SpawnWave(waveNum);
     }
 
-
+    void SpawnWave(int enemyNum)
+    {
+        Instantiate(powerUpPrefab, GenerateSpawnPosition(), powerUpPrefab.transform.rotation);
+        for (int i = 0; i < enemyNum; i++)
+        {    
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+    }
     Vector3 GenerateSpawnPosition()
     {
         float xPos = Random.Range(-spawnRange, spawnRange);
@@ -21,9 +34,15 @@ public class SpawnManager : MonoBehaviour
         Vector3 spawnPos = new Vector3(xPos, enemyPrefab.transform.position.y, ZPos);
         return spawnPos;
     }
+    
     // Update is called once per frame
     void Update()
     {
-       
+        enemyCount = FindObjectsOfType<Enemy>().Length; 
+        if (enemyCount == 0)
+        {
+            waveNum++;
+            SpawnWave(waveNum);
+        }
     }
 }
--- Assets/scripts/enemy.cs
+++ Assets/scripts/enemy.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class enemy : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
     Rigidbody enemyRB;
     GameObject player;
     public float speed = 100.0f;
+
+    private float yBoundary = -15.0f;
+
+    public int Length { get; internal set; }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +23,10 @@ public class enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.y< yBoundary)
+        {
+            Destroy(gameObject);
+        }
 
         Vector3 seekDirection = (player.transform.position - transform.position).normalized;
         enemyRB.AddForce(seekDirection * speed * Time.deltaTime);
